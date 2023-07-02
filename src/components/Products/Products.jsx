9463946253e745ddeb99f890b25604fdb2263f64
nvtungIT <tung.nv.work@gmail.com>
2023-07-02 22:51:44 +0700

    upadate all product

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -5,8 +5,37 @@ import ImgProduct3 from "../../images/product3.jpg";
 import IconEdit from "../../assets/icon-edit.svg";
 import IconDelete from "../../assets/icon-trash-black.svg";
 import { Link } from "react-router-dom";
+import axios from "axios";
+import { useEffect, useState } from "react";
 
 const Products = () => {
+  const [productList, setProductList] = useState([]);
+  
+  const [isLoading, setIsLoading] = useState(true);
+   
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const  response  = await axios.get('http://localhost:3000/product/get');
+        console.log(response.data.data)
+        setProductList(JSON.parse(response.data.data))
+      
+      } catch (error) {
+        console.error(error)
+      }
+      setIsLoading(false);
+    };
+
+    fetchData();
+  }, []);
+
+
+  
+ 
+  if (isLoading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="right">
       <div className="right__content">
@@ -14,85 +43,54 @@ const Products = () => {
         <p className="right__desc">Xem sản phẩm</p>
         <div className="right__table">
           <div className="right__tableWrapper">
-            <table>
-              <thead>
-                <tr>
-                  <th>STT</th>
-                  <th>Tên sản phẩm</th>
-                  <th>Hình ảnh</th>
-                  <th>Giá SP</th>
-                  <th>Đã bán</th>
-                  <th>Từ khoá</th>
-                  <th>Thời gian</th>
-                  <th>Sửa</th>
-                  <th>Xoá</th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr>
-                  <td data-label="STT">1</td>
-                  <td data-label="Tên sản phẩm"> iPhone 14 Pro Max 128GB</td>
-                  <td data-label="Hình ảnh">
-                    <img src={ImgProduct1} alt="" />
-                  </td>
-                  <td data-label="Giá SP">26.680.000₫</td>
-                  <td data-label="Đã bán">1</td>
-                  <td data-label="Từ khoá">mobile</td>
-                  <td data-label="Thời gian">2020-07-13 21:31:05</td>
-                  <td data-label="Sửa" className="right__iconTable">
-                    <Link to="/template/editProduct/iphone14-pro-max-128gb">
-                      <img src={IconEdit} alt="" />
-                    </Link>
-                  </td>
-                  <td data-label="Xoá" className="right__iconTable">
-                    <img src={IconDelete} alt="" />
-                  </td>
-                </tr>
-                <tr>
-                  <td data-label="STT">2</td>
-                  <td data-label="Tên sản phẩm">iPhone 13 Pro Max 128GB</td>
-                  <td data-label="Hình ảnh">
-                    <img src={ImgProduct2} alt="" />
-                  </td>
-                  <td data-label="Giá SP">18.500.000₫</td>
-                  <td data-label="Đã bán">0</td>
-                  <td data-label="Từ khoá">mobi</td>
-                  <td data-label="Thời gian">2020-07-13 22:19:01</td>
-                  <td data-label="Sửa" className="right__iconTable">
-                    <Link to="/template/editProduct/iphone-13-pro-max-128gb">
-                      <img src={IconEdit} alt="" />
-                    </Link>
-                  </td>
-                  <td data-label="Xoá" className="right__iconTable">
-                    <img src={IconDelete} alt="" />
-                  </td>
-                </tr>
-                <tr>
-                  <td data-label="STT">3</td>
-                  <td data-label="Tên sản phẩm">Samsung S21 FE</td>
-                  <td data-label="Hình ảnh">
-                    <img src={ImgProduct3} alt="" />
-                  </td>
-                  <td data-label="Giá SP">12.490.000 ₫</td>
-                  <td data-label="Đã bán">1</td>
-                  <td data-label="Từ khoá">mobi</td>
-                  <td data-label="Thời gian">2020-07-13 21:30:41</td>
-                  <td data-label="Sửa" className="right__iconTable">
-                    <Link to="/template/editProduct/samsung-s21-fe">
-                      <img src={IconEdit} alt="" />
-                    </Link>
-                  </td>
-                  <td data-label="Xoá" className="right__iconTable">
-                    <img src={IconDelete} alt="" />
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+
+            
+            
+              <table>
+                <thead>
+                  <tr>
+                    <th>STT</th>
+                    <th>Tên sản phẩm</th>
+                    <th>Hình ảnh</th>
+                    <th>Giá SP</th>
+                    <th>Loại</th>
+                    <th>Sửa</th>
+                    <th>Xoá</th>
+                  </tr>
+                </thead>
+                
+                  {productList.map((product, index) => (
+
+                    <tr key={product.id}>
+                      <td data-label= "STT">{index+1}</td>
+                      <td data-label="Tên sản phẩm">{product.name}</td>
+                      <td data-label="Hình ảnh">
+                        <img style={{width: 200}} src={product.image} alt="" />
+                      </td>
+                      <td data-label="Giá SP">{product.price}</td>
+                     
+                      <td data-label="Loại">{product.categoryName}</td>
+                  
+                      <td data-label="Sửa" className="right__iconTable">
+                        <Link to={`/template/editProduct/${product.id}}`}>
+                          <img src={IconEdit} alt="" />
+                        </Link>
+                      </td>
+                      <td data-label="Xoá" className="right__iconTable">
+                        <button><img  src={IconDelete} alt="" /></button>
+                        
+                      </td>
+                    </tr>
+                  ))}
+                
+              </table>
+            
           </div>
         </div>
       </div>
     </div>
   );
 };
+
 
 export default Products;
